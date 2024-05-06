79bf390b331dfa85a6d094f18c90b4ce96918768
ahmedbentabet <bentabet841@gmail.com>
2024-05-06 17:34:19 +0000

    Fix the code

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -10,7 +10,16 @@ file { '/etc/nginx/sites-available/default':
   ensure  => present,
   content => "\
 server {
+    listen 80;
+    server_name localhost;
+
+    # Custom HTTP header
     add_header X-Served-By ${hostname};
+
+    location / {
+        root /var/www/html;
+        index index.html index.htm;
+    }
 }",
   notify  => Service['nginx'],  # Notify Nginx service to restart when the file changes
 }
